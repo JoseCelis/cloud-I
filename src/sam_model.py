441cb3dc6d23a525b1f434ab64d523f33a66e40f
anamabo <anamabo3@gmail.com>
2024-06-23 09:01:37 +0200

    Correr SAM model

--- src/sam_model.py
+++ src/sam_model.py
@@ -7,15 +7,15 @@ from ultralytics import settings
 sys.path.append(os.getcwd())
 
 data_config = os.path.join(os.getcwd(), "Dataset", "dataset.yaml")
-epochs = 10
+epochs = 15
 
-# path_model_runs = "model/SAM"
-# os.makedirs(path_model_runs, exist_ok=True)
-#
-# settings.update({
-#     "runs_dir": path_model_runs,
-#     "weights_dir": path_model_runs,
-# })
+path_model_runs = os.path.join(os.getcwd(), "Dataset", "output", "SAM")
+os.makedirs(path_model_runs, exist_ok=True)
+
+settings.update({
+    "runs_dir": path_model_runs,
+    "weights_dir": path_model_runs,
+})
 
 model = YOLO("yolov8n-seg.pt")  # pre-trained model
 results = model.train(data=data_config, epochs=epochs)
